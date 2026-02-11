a04e68804c65c0ea4055c58e2148c33fb7bd4b86
catsu <lupus5523@gmail.com>
2026-02-11 16:52:07 +0100

    feat: implement load configuration API

--- load/load_cfg.c
+++ load/load_cfg.c
@@ -4,3 +4,8 @@ static const load_cfg_t load_config[LOAD_MAX] =
 {
     /* Add your load configurations here */
 };
+
+const load_cfg_t * load_get_config(load_id_t id)
+{
+	return &load_config[id];
+}
