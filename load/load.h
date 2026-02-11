0a50f316ac8d4e5cf2068fe7d27707f123306151
catsu <lupus5523@gmail.com>
2026-02-11 16:56:50 +0100

    feat: add load API

--- load/load.h
+++ load/load.h
@@ -18,4 +18,8 @@ typedef struct
 	load_status_t (*toggle)(load_hw_t hw);
 } load_hal_t;
 
+load_status_t load_init(load_id_t id, load_hw_t hw, const load_hal_t *hal, const load_cfg_t *cfg);
+load_status_t load_set(load_id_t id, load_state_t state);
+load_status_t load_toggle(load_id_t id);
+
 #endif
