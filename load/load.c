3583ef703659cc33aff5585dbabfa5e9530b4551
catsu <lupus5523@gmail.com>
2026-02-11 17:00:42 +0100

    feat: add load output state transition table for different triggers

--- load/load.c
+++ load/load.c
@@ -1,6 +1,8 @@
 #include <stddef.h>
 #include "load.h"
 
+#define LOAD_TRIGGERS	2U
+
 typedef struct
 {
 	load_hw_t hw;
@@ -9,3 +11,9 @@ typedef struct
 } load_instance_t;
 
 static load_instance_t load_instances[LOAD_MAX];
+
+static const load_state_t state_for_trigger[][LOAD_TRIGGERS] =
+{
+	{LOAD_ON, LOAD_OFF},
+	{LOAD_OFF, LOAD_ON}
+};
